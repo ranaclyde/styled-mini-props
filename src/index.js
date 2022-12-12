6cda4aebc65571466ff56f1c40017cd22b8d052f
Matías E Sanhueza <manoloesanhueza@gmail.com>
2022-12-12 17:57:04 -0300

    Fixs module exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import { createMediaQueries } from "./css-media-queries/create-media-queries";
-import { cssHovProperties } from "./css-media-queries/css-hov-properties";
-
-module.export = {
-  createMediaQueries,
-  cssHovProperties,
-}
+export * from "./css-media-queries/create-media-queries";
+export * from "./css-media-queries/css-hov-properties";
