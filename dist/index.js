b92fba4a83de712d313ba770113cffe49299cb4e
Matías E Sanhueza <manoloesanhueza@gmail.com>
2022-12-12 15:38:55 -0300

    Adds dist folder

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _createMediaQueries = require("./css-media-queries/create-media-queries");
+var _cssHovProperties = require("./css-media-queries/css-hov-properties");
+module["export"] = {
+  createMediaQueries: _createMediaQueries.createMediaQueries,
+  cssHovProperties: _cssHovProperties.cssHovProperties
+};
