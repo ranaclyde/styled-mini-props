f5b1dd363e8cc615d0d386390614779e38549a9e
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-02-12 17:21:04 -0300

    Fixes a litle bug with imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { css } from 'styled-components';
 import { createMediaQueries } from './css-media-queries/create-media-queries';
 import { cssHovProperties } from './css-media-queries/css-hov-properties';
-import { breakpoints as breakpointsDefault } from './breakpoints';
+import { breakpoints as breakpointsDefault } from './css-media-queries/breakpoints';
 
 // Breakpoints
 let breakpoints = { ...breakpointsDefault };
